41992cdb8d9b31d1c12fa0c99ab8ffd86bf8534a
Darren-Stander <darrenjadestander@gmail.com>
2024-06-26 17:33:26 +0200

    added comments to my code to show what was all done

--- Class folder/Recipe.cs
+++ Class folder/Recipe.cs
@@ -1,55 +1,64 @@
-﻿using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;  // Importing the necessary libraries for collections
+using System.Linq;  // Importing LINQ for querying collections
 
 namespace RecipeApp
 {
+    // This class represents a recipe, including its name, ingredients, and steps
     public class Recipe
     {
+        // Private fields to hold the ingredients and steps of the recipe
         private List<Ingredient> ingredients;
         private List<RecipeStep> steps;
 
+        // Constructor to initialize the recipe with a name
         public Recipe(string name)
         {
-            Name = name;
-            ingredients = new List<Ingredient>();
-            steps = new List<RecipeStep>();
+            Name = name;  // Setting the name of the recipe
+            ingredients = new List<Ingredient>();  // Initializing the list of ingredients
+            steps = new List<RecipeStep>();  // Initializing the list of steps
         }
 
+        // Public property to get or set the name of the recipe
         public string Name { get; set; }
 
+        // Public property to get or set the list of ingredients
         public List<Ingredient> Ingredients
         {
-            get => ingredients;
-            set => ingredients = value;
+            get => ingredients;  // Returning the list of ingredients
+            set => ingredients = value;  // Setting the list of ingredients
         }
 
+        // Public property to get or set the list of steps
         public List<RecipeStep> Steps
         {
-            get => steps;
-            set => steps = value;
+            get => steps;  // Returning the list of steps
+            set => steps = value;  // Setting the list of steps
         }
 
+        // Public property to get a summary of the ingredients
         public string IngredientSummary
         {
             get
             {
-                if (Ingredients == null || Ingredients.Count == 0) return "";
-                return string.Join(", ", Ingredients.Select(i => $"{i.Quantity} {i.Unit} {i.Name}"));
+                if (Ingredients == null || Ingredients.Count == 0) return "";  // Return an empty string if there are no ingredients
+                return string.Join(", ", Ingredients.Select(i => $"{i.Quantity} {i.Unit} {i.Name}"));  // Joining the ingredients into a single string
             }
         }
 
+        // Public property to get a summary of the food groups of the ingredients
         public string FoodGroupSummary
         {
             get
             {
-                if (Ingredients == null || Ingredients.Count == 0) return "";
-                return string.Join(", ", Ingredients.Select(i => i.FoodGroup).Distinct());
+                if (Ingredients == null || Ingredients.Count == 0) return "";  // Return an empty string if there are no ingredients
+                return string.Join(", ", Ingredients.Select(i => i.FoodGroup).Distinct());  // Joining distinct food groups into a single string
             }
         }
 
+        // Public property to get the total calories of the recipe
         public int TotalCalories
         {
-            get { return Ingredients.Sum(i => i.Calories); }
+            get { return Ingredients.Sum(i => i.Calories); }  // Summing up the calories of all ingredients
         }
     }
 }
