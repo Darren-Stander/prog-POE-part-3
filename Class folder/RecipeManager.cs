7eef82ec84e1b785a5ccbafd7831e082a706110f
Darren-Stander <darrenjadestander@gmail.com>
2024-06-26 17:39:30 +0200

    comments for the RecipeManager

--- Class folder/RecipeManager.cs
+++ Class folder/RecipeManager.cs
@@ -1,15 +1,19 @@
-﻿
-using System.Collections.Generic;
+﻿using System.Collections.Generic;  // Importing namespace for collections
 
 namespace RecipeApp
 {
+    // This class is responsible for managing a collection of recipes
     public class RecipeManager
     {
+        // Public field to hold the list of recipes
         public List<Recipe> recipes;
 
+        // Constructor for the RecipeManager class
         public RecipeManager()
         {
-            recipes = new List<Recipe>();
+            recipes = new List<Recipe>();  // Initializing the list of recipes
         }
     }
 }
+
+// End of file
